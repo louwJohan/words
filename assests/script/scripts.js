085a4ce430ea8dbf4a4f41a3070b636c21e71e9d
louwJohan <louwjohanc@gmail.com>
2023-04-19 12:38:30 +0100

    Add function to display words at intervals

--- assests/script/scripts.js
+++ assests/script/scripts.js
@@ -7,8 +7,16 @@
         e.preventDefault();
         let text = localStorage.getItem("word_List")
         wordList = text.split(',')
-        console.log(wordList[2])
-        document.getElementById('word').textContent = wordList
+    
+        setInterval(displayHello, 1000);
+
+        function displayHello() {
+                
+            number = Math.floor(Math.random() * wordList.length);
+    
+            document.getElementById("word").innerText = wordList[number];
+        }
+
       });
     
       document.getElementById('add')?.addEventListener('click', function(e) {
