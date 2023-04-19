027a484e165bf2dfff4c2fe2fa414d79d9036f89
louwJohan <louwjohanc@gmail.com>
2023-04-19 15:44:43 +0100

    Add functionality to practise

--- assests/script/scripts.js
+++ assests/script/scripts.js
@@ -1,27 +1,29 @@
+let wordList = [];
 
-    let wordList = []
+let text = localStorage.getItem("word_List");
+let change = document.getElementById("start").classList;
+wordList = text.split(",");
+function displayWords() {
+  number = Math.floor(Math.random() * wordList.length);
 
+  document.getElementById("word").innerText = wordList[number];
+}
 
+document.getElementById("start")?.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    document.getElementById('start')?.addEventListener('click', function(e) {
-        e.preventDefault();
-        let text = localStorage.getItem("word_List")
-        wordList = text.split(',')
-    
-        setInterval(displayHello, 1000);
+  if ((document.getElementById("start").innerText = "Start")) {
+    let myInterval = setInterval(displayWords, 1000);
+    console.log(myInterval)
+    document.getElementById("start").innerText = "Stop";
+  } else if ((document.getElementById("start").innerText = "Stop")) {
+    clearInterval(myInterval);
+    document.getElementById("start").innerText = "Start";
+  }
+});
 
-        function displayHello() {
-                
-            number = Math.floor(Math.random() * wordList.length);
-    
-            document.getElementById("word").innerText = wordList[number];
-        }
-
-      });
-    
-      document.getElementById('add')?.addEventListener('click', function(e) {
-        e.preventDefault();
-        let words = document.getElementById('words').value
-        localStorage.setItem('word_List', [words])
-      });
-
+document.getElementById("add")?.addEventListener("click", function (e) {
+  e.preventDefault();
+  let words = document.getElementById("words").value;
+  localStorage.setItem("word_List", [words]);
+});
