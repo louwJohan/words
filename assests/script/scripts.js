1af8719527e3eef77355abc6f229da8d01dd7e6b
louwJohan <louwjohanc@gmail.com>
2023-04-20 10:26:29 +0100

    Add time interval setting

--- assests/script/scripts.js
+++ assests/script/scripts.js
@@ -1,8 +1,16 @@
-let wordList = [];
-
 let text = localStorage.getItem("word_List");
-let change = document.getElementById("start").classList;
-wordList = text.split(",");
+let last = text.substr(text.length - 1);
+let wordList = [];
+let time_int = localStorage.getItem("time")
+const startBtn = document.getElementById("start");
+if (last === ","){
+  newList = text.slice(0, text.length - 1)
+  wordList = newList.split(",")
+} else {
+  wordList = text.split(",");
+}
+
+
 function displayWords() {
   number = Math.floor(Math.random() * wordList.length);
 
@@ -10,21 +18,33 @@ function displayWords() {
 }
 
 
-document.getElementById("start")?.addEventListener("click", function (e) {
+startBtn?.addEventListener("click", function (e) {
   e.preventDefault();
 
-  if ((document.getElementById("start").innerText === "Start")) {
-    myInterval = setInterval(displayWords, 1000);
-    console.log(myInterval)
+  if ((startBtn.innerText === "Start")) {
+    myInterval = setInterval(displayWords, time_int);
     document.getElementById("start").innerText = "Stop";
+    startBtn.classList.remove("btn-success");
+    startBtn.classList.add("btn-danger")
   } else if ((document.getElementById("start").innerText === "Stop")) {
     clearInterval(myInterval);
     document.getElementById("start").innerText = "Start";
+    startBtn.classList.remove("btn-danger");
+    startBtn.classList.add("btn-success")
   }
 });
 
 document.getElementById("add")?.addEventListener("click", function (e) {
   e.preventDefault();
-  let words = document.getElementById("words").value;
+  alert("Thank you! Your words were added.")
+  words = document.getElementById("words").value;
   localStorage.setItem("word_List", [words]);
+ 
+});
+
+document.getElementById("add_time")?.addEventListener("click", function (e) {
+  e.preventDefault();
+  alert("Thank you! Your time interval is set.")
+  time = document.getElementById('time').value;
+  localStorage.setItem("time", [time])
 });
