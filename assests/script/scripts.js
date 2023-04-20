214dfeb47a06952a2671f4ebe04ac95a2ab39440
louwJohan <louwjohanc@gmail.com>
2023-04-20 11:37:16 +0100

    validation for the settings

--- assests/script/scripts.js
+++ assests/script/scripts.js
@@ -20,31 +20,52 @@ function displayWords() {
 
 startBtn?.addEventListener("click", function (e) {
   e.preventDefault();
-
-  if ((startBtn.innerText === "Start")) {
-    myInterval = setInterval(displayWords, time_int);
-    document.getElementById("start").innerText = "Stop";
-    startBtn.classList.remove("btn-success");
-    startBtn.classList.add("btn-danger")
-  } else if ((document.getElementById("start").innerText === "Stop")) {
-    clearInterval(myInterval);
-    document.getElementById("start").innerText = "Start";
-    startBtn.classList.remove("btn-danger");
-    startBtn.classList.add("btn-success")
+  if (text.length === 0){
+    alert("Please add words in Settings")
+  } else if (time_int === 0 || time_int === undefined){
+    alert("Please set the time interval in Settings")
+  } else {
+    if ((startBtn.innerText === "Start")) {
+      myInterval = setInterval(displayWords, time_int);
+      startBtn.innerText = "Stop";
+      startBtn.classList.remove("btn-success");
+      startBtn.classList.add("btn-danger")
+    } else if ((document.getElementById("start").innerText === "Stop")) {
+      clearInterval(myInterval);
+      startBtn.innerText = "Start";
+      startBtn.classList.remove("btn-danger");
+      startBtn.classList.add("btn-success")
+    }
   }
+    
+
+
+  
 });
 
 document.getElementById("add")?.addEventListener("click", function (e) {
   e.preventDefault();
-  alert("Thank you! Your words were added.")
+  
   words = document.getElementById("words").value;
-  localStorage.setItem("word_List", [words]);
+  if (words.length === 0 ){
+    alert("Please add words!")
+  } else {
+    localStorage.setItem("word_List", [words]);
+    alert("Thank you! Your words were added.")
+  }
+  
  
 });
 
 document.getElementById("add_time")?.addEventListener("click", function (e) {
   e.preventDefault();
-  alert("Thank you! Your time interval is set.")
+  
   time = document.getElementById('time').value;
-  localStorage.setItem("time", [time])
+  if (time.length === 0){
+    alert("Please set a time interval")
+  } else {
+    localStorage.setItem("time", [time])
+    alert("Thank you! Your time interval is set.")
+  }
+  
 });
